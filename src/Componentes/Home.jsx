d202055837a2b7feb9ca62919f225ed46453eead
MatiasMV <mjmazavega@gmail.com>
2023-07-30 20:29:11 -0300

    arreglo home

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -6,12 +6,13 @@ const Home = () => {
     return (
         <Container className='my-5'>
             <Row>
-            <Col><h1 style={{color:"#F2E6DF", fontFamily:"Verdana"}}>Bienvenido a nuestro bistró en La Matanza, Buenos Aires. Disfruta de auténticos sabores caseros y la calidez familiar. Nuestro menú sencillo y delicioso te llevará a un viaje culinario. Aquí, cada comensal es parte de nuestra familia.</h1></Col>
+            <Col><div style={{backgroundColor: "rgba(0,0,0,.5)", borderRadius: "10px"}}><h1 className="my-3 mx-3" style={{color:"#F2E6DF", fontFamily:"Verdana"}}>Bienvenido a nuestro bistró en La Matanza, Buenos Aires. Disfruta de auténticos sabores caseros y la calidez familiar. Nuestro menú sencillo y delicioso te llevará a un viaje culinario. Aquí, cada comensal es parte de nuestra familia. </h1></div></Col>
             <Col><img src="img/logowhite.png" height="350px"></img></Col>
-            <Col><h1 style={{color:"#F2E6DF", fontFamily:"Verdana", textAlign:"right"}}>Comparte momentos especiales con tus seres queridos en un ambiente relajado. Te esperamos con los brazos abiertos para brindarte una experiencia gastronómica inolvidable. ¡Ven y sé parte de nuestra historia!</h1></Col>
+            <Col><div style={{backgroundColor: "rgba(0,0,0,.5)", borderRadius: "10px"}}><h1 className="my-3 mx-3" style={{color:"#F2E6DF", fontFamily:"Verdana", textAlign:"right"}}>Comparte momentos especiales con tus seres queridos en un ambiente relajado. Te esperamos con los brazos abiertos para brindarte una experiencia gastronómica inolvidable. ¡Ven y sé parte de nuestra historia!</h1></div></Col>
             </Row>
+            <hr></hr>
             <Row>
-            <h1 className="mt-5" style={{color:"#F2E6DF", fontFamily:"Verdana"}}>Podés revisar nuestro menú en las pestañas superiores, y podés pedir nuestro delivery desde Rappi, Pedidos Ya o usando mercado pago a través de nuestro whatsapp. ¡Buen Provecho!</h1>
+            <div style={{backgroundColor: "rgba(0,0,0,.5)", borderRadius: "10px"}}><h1 className="my-3 mx-5" style={{color:"#F2E6DF", fontFamily:"Verdana"}}>Podés revisar nuestro menú en las pestañas superiores, y podés pedir nuestro delivery desde Rappi, Pedidos Ya o usando mercado pago a través de nuestro whatsapp. ¡Buen Provecho!</h1></div>
             </Row>
         </Container>
     )
