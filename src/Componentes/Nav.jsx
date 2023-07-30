2f82b2b9d740a27f78ef51a7dfd7a3bc1da8b092
MatiasMV <mjmazavega@gmail.com>
2023-07-30 19:55:12 -0300

    sin acento

--- src/Componentes/Nav.jsx
+++ src/Componentes/Nav.jsx
@@ -10,7 +10,7 @@ const Navegacion = () => {
           <Row className="justify-content-md-center my-4"><Col><img src="img/type.png" width="600px"></img></Col></Row>
           <hr></hr>
           <Row>
-          <Col><div className="m-1"><Link to="/"><img src="/public/img/botónlanding.png" alt="..." width="200px"></img></Link></div></Col>
+          <Col><div className="m-1"><Link to="/"><img src="/public/img/botonlanding.png" alt="..." width="200px"></img></Link></div></Col>
           <Col><div className="m-1"><Link to="/MenuEstandar"><img src="../../public/img/botónestándar.png" alt="..." width="200px"></img></Link></div></Col>
           <Col><div className="m-1"><Link to="/MenuVeggie"><img src="../../public/img/botónveggie.png" alt="..." width="200px"></img></Link></div></Col>
           <Col><div className="m-1"><Link to="/Postres"><img src="../../public/img/botónpostres.png" alt="..." width="200px"></img></Link></div></Col>
